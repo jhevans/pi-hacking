3b0bd5ad02dd88e5e577472ea09f9dc84a332bb6
John Evans <jhevans28@gmail.com>
2020-05-12 21:57:00 +0000

    Add event based LED button script

---
+++ LED3.py
@@ -0,0 +1,23 @@
+# Event based switch on PIN 10 flashes LED at pin 12 twice
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(12,GPIO.OUT)
+
+def button_callback(channel):
+    print "Blink blink!"
+    
+    for _ in range(2):
+        GPIO.output(12,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(12,GPIO.LOW)
+        time.sleep(0.5)
+
+GPIO.setup(10,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
+
+while(True):
+    pass
+
