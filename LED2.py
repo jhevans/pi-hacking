106ab8bdc7e50dae6f6dea9483ae3d0a65ec9d4f
John Evans <jhevans28@gmail.com>
2023-05-07 22:44:25 +0100

    Fix script which just wasn't working

--- LED2.py
+++ LED2.py
@@ -1,20 +1,24 @@
-# Use a switch on GPIO pin 10 to switch an LED on pin 12
-
-import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(10,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-while(True):
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button pushed")
-        print("LED on")
-        GPIO.output(12,GPIO.HIGH)
-    else:
-        print("No button")
-        print("LED off")
-        GPIO.output(12,GPIO.LOW)
+import RPi.GPIO as GPIO
 
+# Pins definitions
+btn_pin = 4
+led_pin = 12
 
+# Set up pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(btn_pin, GPIO.IN)
+GPIO.setup(led_pin, GPIO.OUT)
+
+# If button is pushed, light up LED
+try:
+    while True:
+        if GPIO.input(btn_pin):
+            GPIO.output(led_pin, GPIO.LOW)
+        else:
+            GPIO.output(led_pin, GPIO.HIGH)
+
+# When you press ctrl+c, this will be called
+finally:
+    GPIO.cleanup()
 
